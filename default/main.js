cb6beb49b041011294deb23f7d69879bf477422b
GenDeathorw <kennywd.lmt@gmail.com>
2017-02-02 12:46:28 -0600

    Temp fix incase all big harvesters die

--- default/main.js
+++ default/main.js
@@ -16,39 +16,49 @@ module.exports.loop = function () {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     //console.log('Harvesters: ' + harvesters.length);
 
-    if(harvesters.length < 5) 
+    if(harvesters.length < 4) 
     {
-        var newName = Game.spawns['Gehross'].createCreep([WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'harvester'});
+        var newName = Game.spawns['Gehross'].createCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
     
-    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    //console.log('Builders: ' + harvesters.length);
-    
-    if(builders.length < 4) 
+    // If all harvesters die, spawn a simple harvester
+    if(harvesters.length == 0) 
     {
-        var newName = Game.spawns['Gehross'].createCreep([WORK,WORK,WORK, CARRY,CARRY,MOVE,MOVE], undefined, {role: 'builder'});
-        console.log('Spawning new builder: ' + newName);
+        var newName = Game.spawns['Gehross'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
     }
     
-    var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    //console.log('Builders: ' + harvesters.length);
-    
-    if(upgrader.length < 4) 
+    // Dont spawn anything until at least 2 harvesters exist
+    if(harvesters.length >= 2)
     {
-        var newName = Game.spawns['Gehross'].createCreep([WORK,WORK,WORK,CARRY,CARRY,CARRY, MOVE, MOVE], undefined, {role: 'upgrader'});
-        console.log('Spawning new upgrader: ' + newName);
+         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+        //console.log('Builders: ' + harvesters.length);
+    
+        if(builders.length < 4) 
+        {
+            var newName = Game.spawns['Gehross'].createCreep([WORK,WORK,WORK, CARRY,CARRY,MOVE,MOVE], undefined, {role: 'builder'});
+            console.log('Spawning new builder: ' + newName);
+        }
+    
+        var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+        //console.log('Builders: ' + harvesters.length);
+    
+        if(upgrader.length < 4) 
+        {
+            var newName = Game.spawns['Gehross'].createCreep([WORK,WORK,WORK,CARRY,CARRY,CARRY, MOVE, MOVE], undefined, {role: 'upgrader'});
+            console.log('Spawning new upgrader: ' + newName);
+        }
+    
+        var repairer = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+        //console.log('Builders: ' + harvesters.length);
+    
+        if(repairer.length < 2) 
+        {
+            var newName = Game.spawns['Gehross'].createCreep([WORK, WORK, CARRY, CARRY,MOVE, MOVE, MOVE], undefined, {role: 'repairer'});
+            console.log('Spawning new repairer: ' + newName);
+        }
     }
-    
-    var repairer = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
-    //console.log('Builders: ' + harvesters.length);
-    
-    if(repairer.length < 2) 
-    {
-        var newName = Game.spawns['Gehross'].createCreep([WORK, WORK, CARRY, CARRY,MOVE, MOVE, MOVE], undefined, {role: 'repairer'});
-        console.log('Spawning new repairer: ' + newName);
-    }
-    
     
     
     for(var name in Game.creeps) 
